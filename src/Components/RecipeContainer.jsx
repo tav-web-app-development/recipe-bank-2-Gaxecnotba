2ac776e020e8053b33bb8a94f5a7567670cd1b87
Sebastian Rojas <sebasleprodevj12@gmail.com>
2024-04-16 18:47:45 -0400

    Fixed errors

--- src/Components/RecipeContainer.jsx
+++ src/Components/RecipeContainer.jsx
@@ -9,14 +9,13 @@ export default function RecipeContainer({ recipe, onEdit, onDelete }) {
   };
 
   const [edit, setEdit] = useState(false);
-  const [editRecipe, setEditRecipe] = useState({ ...recipe });
 
   const handleEdit = () => {
     setEdit(true);
   };
 
   const handleSave = () => {
-    onEdit(editRecipe);
+    onEdit(recipe);
     setEdit(false);
   };
 
@@ -26,90 +25,113 @@ export default function RecipeContainer({ recipe, onEdit, onDelete }) {
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
-    setEditRecipe({
-      ...editRecipe,
+    onEdit({
+      ...recipe,
       [name]: value,
     });
   };
 
   return (
     <div className="recipe-container">
-      {edit ? (
-        <div className="edit-recipe">
-          <label htmlFor="title">
-            Title:
-            <input
-              type="text"
-              id="title"
-              name="title"
-              value={editRecipe.title}
-              onChange={handleInputChange}
-            />
-          </label>
+      <div className="recipe">
+        {edit ? (
+          <textarea
+            type="text"
+            name="title"
+            value={recipe.title}
+            onChange={handleInputChange}
+          >
+            {recipe.title}
+          </textarea>
+        ) : (
+          <h2>{recipe.title}</h2>
+        )}
+        ;
+        {edit ? (
           <label htmlFor="description">
             Description:
-            <input
+            <textarea
               type="text"
               id="description"
               name="description"
-              value={editRecipe.description}
+              value={recipe.description}
               onChange={handleInputChange}
-            />
+            >
+              {recipe.description}
+            </textarea>
           </label>
-          <label htmlFor="ingredients">
-            Ingredients:
-            <textarea
-              id="ingredients"
-              name="ingredients"
-              value={editRecipe.ingredients}
-              onChange={handleInputChange}
-              cols="30"
-              rows="10"
-            ></textarea>
-          </label>
-          <label htmlFor="directions">
-            Directions:
-            <textarea
-              id="directions"
-              name="directions"
-              value={editRecipe.directions}
-              onChange={handleInputChange}
-              cols="60"
-              rows="20"
-            ></textarea>
-          </label>
-          <button type="button" onClick={handleSave}>
-            Save
-          </button>
-        </div>
-      ) : (
-        <div className="recipe">
-          <h2>{recipe.title}</h2>
+        ) : (
           <p>
             <strong>Description:</strong>
             {recipe.description}
           </p>
+        )}
+        {edit ? (
+          <label htmlFor="ingredients">
+            Ingredients:
+            <textarea
+              name="ingredients"
+              id="ingredients"
+              cols="30"
+              rows="10"
+              value={recipe.ingredients}
+              onChange={handleInputChange}
+            >
+              {recipe.ingredients}
+            </textarea>
+          </label>
+        ) : (
           <p>
             <strong>Ingredients:</strong> {recipe.ingredients}
           </p>
+        )}
+        {edit ? (
+          <label htmlFor="directions">
+            Direction:
+            <textarea
+              id="directions"
+              name="directions"
+              value={recipe.directions}
+              onChange={handleInputChange}
+              cols="60"
+              rows="20"
+            >
+              {recipe.directions}
+            </textarea>
+          </label>
+        ) : (
           <p>
             <strong>Directions:</strong> {recipe.directions}
           </p>
-          <img
-            src={recipe.photoUrl}
-            alt={recipe.title}
-            width={300}
-            height={300}
-          />
-        </div>
-      )}
+        )}
+        <img
+          src={recipe.photoUrl}
+          alt={recipe.title}
+          width={300}
+          height={300}
+        />
+      </div>
       <div className="button">
-        <button type="button" onClick={handleEdit}>
-          Edit
-        </button>
-        <button type="button" onClick={handleDelete}>
-          Delete
-        </button>
+        {edit ? (
+          <>
+            <button type="button" onClick={handleSave}>
+              Save
+            </button>
+            <button type="button" onClick={() => setEdit(false)}>
+              {" "}
+              Cancel
+            </button>
+          </>
+        ) : (
+          <>
+            <button type="button" onClick={handleEdit}>
+              Edit
+            </button>
+            <button type="button" onClick={handleDelete}>
+              Delete
+            </button>
+          </>
+        )}
       </div>
     </div>
   );
